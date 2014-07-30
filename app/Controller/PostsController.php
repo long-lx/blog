490a4b29f9a8117f4e2b5d945577db69fb8c30c3
long-lx <long_lx@septeni-technology.jp>
2014-07-30 23:51:10 +0700

    add function

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -18,6 +18,17 @@ class PostsController extends AppController {
         }
         $this->set('post', $post);
     }
+    
+    public function add() {
+        if ($this->request->is('post')) {
+            $this->Post->create();
+            if ($this->Post->save($this->request->data)) {
+                $this->Session->setFlash(__('Your post has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            }
+            $this->Session->setFlash(__('Unable to add your post.'));
+        }
+    }
 }
 
 /* 
