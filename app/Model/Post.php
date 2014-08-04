99960776164277eb45c895087b0bdea2d3d258e4
long-lx <long_lx@septeni-technology.jp>
2014-08-04 21:13:06 +0700

    update post

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -1,5 +1,17 @@
 <?php
 
+class Post extends AppModel {
+    
+    public $validate = array(
+        'title' => array(
+            'rule' => 'notEmpty'
+        ),
+        'body' => array(
+            'rule' => 'notEmpty'
+        )
+    );
+}
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
