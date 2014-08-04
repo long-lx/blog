359a5d3913b97f75a71ce0ddd12ce16f35fbb72e
long-lx <long_lx@septeni-technology.jp>
2014-08-04 22:49:27 +0700

    next next

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -21,6 +21,7 @@ class PostsController extends AppController {
     
     public function add() {
         if ($this->request->is('post')) {
+            $this->request->data['Post']['user_id'] = $this->Auth->user('id');
             $this->Post->create();
             if ($this->Post->save($this->request->data)) {
                 debug($data);
@@ -66,7 +67,25 @@ class PostsController extends AppController {
         );
         return $this->redirect(array('action' => 'index'));
     }
-}
+   }
+   
+   public function isAuthorized($user) {
+    // All registered users can add posts
+    if ($this->action === 'add') {
+        return true;
+    }
+
+    // The owner of a post can edit and delete it
+    if (in_array($this->action, array('edit', 'delete'))) {
+        $postId = (int) $this->request->params['pass'][0];
+        if ($this->Post->isOwnedBy($postId, $user['id'])) {
+            return true;
+        }
+    }
+
+    return parent::isAuthorized($user);
+   }
+
 }
 
 /* 
